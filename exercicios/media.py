35072454c51930462605686fd612812328d8f103
Koucher <davikmach@gmail.com>
2026-01-18 20:14:37 -0300

    exercicio da media e do peso

---
+++ exercicios/media.py
@@ -0,0 +1,8 @@
+# Faça um programa que receba três notas, calcule e mostre a média aritmética entre elas.
+n1 = float(input("Insira a 1º número: "))
+n2 = float(input("Insira o 2º número: "))
+n3 = float(input("Insira o 3º número: "))
+
+media = (n1 + n2 + n3) / 3
+
+print(f"Média final: {media}")
---
+++ exercicios/peso.py
@@ -0,0 +1,6 @@
+# Faça um programa que receba o peso de uma pessoa em quilos,
+# calcule e mostre esse peso em gramas.
+
+def kg_g(quilos): return quilos * 1000
+
+print(kg_g(80))
